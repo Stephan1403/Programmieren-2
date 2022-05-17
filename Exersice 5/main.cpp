9337500f282f73e9734eb6f5d875c17bbf11e9ce
Stephan <Stephanf2003>
2022-05-17 11:43:00 +0200

    remove output

--- Exersice 5/main.cpp
+++ Exersice 5/main.cpp
@@ -4,27 +4,9 @@
 #include "tem_array.cpp"
 
 
+
 int main(){
 
-    //array
-    array* a = new array(3);
 
-    std::cout << "Array size: " << a->getSize() << std::endl;
-    a->set(1, 2);
-    a->resize(2);
-    std::cout << "Array size: " << a->getSize() << std::endl;
-    std::cout << "Array at index 1: " << a->get(1) << std::endl;
-    
-    
-    
 
-    //tem_array
-    tem_array<int>* t = new tem_array<int>(5);
-
-    t->set(4, 5);
-    std::cout << "Tem-array size: " << t->getSize() << std::endl;
-    t->resize(3);
-    std::cout << "Tem-array size: " << t->getSize() << std::endl;
-    std::cout << "Array at index 4: " << t->get(4) << std::endl;
-    
 }
