4c62ba6842c366170bebda127f211e5eeb39db73
Stephan <Stephanf2003>
2022-05-30 01:21:01 +0200

    updated testing

--- Threads/main.cpp
+++ Threads/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 #include <string>
+
 #include <thread>
 
 #include "Line.cpp"
@@ -8,7 +9,9 @@
 #define clear() printf("\033[H\033[J")
 
 
-
+void test(int a){
+    std::cout << a << std::endl;
+}
 
 
 int main(){
@@ -22,6 +25,22 @@ int main(){
     //word lines
     Line* w_line1 = new Line(75, " Hello world ");
     Line* w_line2 = new Line(75, " This is the first result using threads ");
+
+w_line1->set_d_left();
+    w_line1->print_animation(2);
+
+    
+
+    //define threads
+    //std::thread t (&(Line::print_animation), &s_line1, 1);
+    //t_s1.join();
+
+
+    //std::thread t_w1(w_line1->print_animation, 2);
+
+
+    //t_s1.join();
+    //t_w1.join();
 
 }
 
